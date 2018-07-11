4dc25042ef18f330409ae2d4c057871cbf109aa9
KhoiND <khoindse04028@fpt.edu.vn>
2018-07-11 22:46:00 +0700

    KhoiND

---
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/Direction.java
@@ -0,0 +1,16 @@
+package com.example.hung.fparking;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class Direction extends FragmentActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction);
+    }
+
+}
---
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/config/Constants.java
@@ -0,0 +1,13 @@
+package com.example.hung.fparking.config;
+
+public class Constants {
+    public static final String API_URL = "http://192.168.119.230:9000/tat/";
+    public static final String PUSHER_KEY = "d8e15d0b0ecad0c93a5e";
+    public static final String PUSHER_CHANNEL = "Fparking";
+    public static final String PUSHER_ORDER_FOR_BOOKING = "ORDER_FOR_BOOKING";
+    public static final String PUSHER_CHECKIN_FOR_BOOKING = "CHECKIN_FOR_BOOKING";
+    public static final String PUSHER_CHECKOUT_FOR_BOOKING = "CHECKOUT_FOR_BOOKING";
+    public static final String PUSHER_CANCEL_FROM_DRIVER = "CANCEL_FROM_DRIVER";
+
+    //Phone Validation pattern
+}
