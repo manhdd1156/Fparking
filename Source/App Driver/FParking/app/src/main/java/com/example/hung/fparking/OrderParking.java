878b86b7cebcc46de4d571b8d1ad601db2a84693
Hung Nguyen <hungnvse04023@fpt.edu.vn>
2018-07-14 01:05:23 +0700

    update hung nguyen

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/OrderParking.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/OrderParking.java
@@ -2,14 +2,53 @@ package com.example.hung.fparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+import com.synnapps.carouselview.ViewListener;
 
 public class OrderParking extends AppCompatActivity {
+    CarouselView customCarouselView;
 
+    int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
+    String[] sampleNetworkImageURLs = {
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image1&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image2&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image3&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image4&txt=350%C3%97150&w=350&h=150",
+            "https://placeholdit.imgix.net/~text?txtsize=15&txt=image5&txt=350%C3%97150&w=350&h=150"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_parking);
-
+        customCarouselView = (CarouselView) findViewById(R.id.customCarouselView);
+        customCarouselView.setPageCount(sampleImages.length);
+        customCarouselView.setSlideInterval(2500);
+        customCarouselView.setViewListener(viewListener);
     }
 
+    // To set simple images
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+
+            Picasso.with(getApplicationContext()).load(sampleNetworkImageURLs[position]).placeholder(sampleImages[0]).error(sampleImages[3]).fit().centerCrop().into(imageView);
+
+            //imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+    // To set custom views
+    ViewListener viewListener = new ViewListener() {
+        @Override
+        public View setViewForPosition(int position) {
+            View customView = getLayoutInflater().inflate(R.layout.view_custom, null);
+            ImageView fruitImageView = (ImageView) customView.findViewById(R.id.fruitImageView);
+            fruitImageView.setImageResource(sampleImages[position]);
+            return customView;
+        }
+    };
 }
