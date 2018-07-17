ab285f45c7cfb1f3e83142f91551f258cd3256ae
KhoiND <khoindse04028@fpt.edu.vn>
2018-07-18 01:58:31 +0700

    KhoiND

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/Direction.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/Direction.java
@@ -3,6 +3,7 @@ package com.example.hung.fparking;
 import android.app.ProgressDialog;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.hung.fparking.entity.Route;
 import com.example.hung.fparking.model.DirectionFinderListener;
@@ -11,11 +12,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.Polyline;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Direction extends FragmentActivity implements OnMapReadyCallback, DirectionFinderListener {
 
-
+    private GoogleMap mMap;
+    private Button btnFindPath;
+    private List<Marker> originMarkers = new ArrayList<>();
+    private List<Marker> destinationMarkers = new ArrayList<>();
+    private List<Polyline> polylinePaths = new ArrayList<>();
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
