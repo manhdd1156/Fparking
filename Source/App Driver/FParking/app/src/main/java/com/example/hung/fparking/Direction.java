834fe68fbcebba346bb0aa13161edb0b0f02ee7f
KhoiND <khoindse04028@fpt.edu.vn>
2018-07-18 02:50:59 +0700

    KhoiND

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/Direction.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/Direction.java
@@ -1,21 +1,36 @@
 package com.example.hung.fparking;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.hung.fparking.asynctask.IAsyncTaskHandler;
+import com.example.hung.fparking.asynctask.ParkingInforTask;
+import com.example.hung.fparking.dto.ParkingDTO;
 import com.example.hung.fparking.entity.Route;
+import com.example.hung.fparking.model.DirectionFinder;
 import com.example.hung.fparking.model.DirectionFinderListener;
+import com.example.hung.fparking.model.GPSTracker;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Direction extends FragmentActivity implements OnMapReadyCallback, DirectionFinderListener {
+public class Direction extends FragmentActivity implements OnMapReadyCallback, DirectionFinderListener, IAsyncTaskHandler {
 
     private GoogleMap mMap;
     private Button btnFindPath;
@@ -24,14 +39,32 @@ public class Direction extends FragmentActivity implements OnMapReadyCallback, D
     private List<Polyline> polylinePaths = new ArrayList<>();
     private ProgressDialog progressDialog;
 
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mPreferencesEditor;
+
+    ArrayList<ParkingDTO> parkingDTOS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // khởi tạo shareRePreference
+        mPreferences = getSharedPreferences("driver", 0);
+        mPreferencesEditor = mPreferences.edit();
+
+        ParkingInforTask parkingInforTask = new ParkingInforTask("2", "pi", this);
+        parkingInforTask.execute();
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
     }
 
@@ -60,7 +93,57 @@ public class Direction extends FragmentActivity implements OnMapReadyCallback, D
     }
 
     @Override
-    public void onDirectionFinderSuccess(List<Route> route) {
+    public void onDirectionFinderSuccess(List<Route> routes) {
+        progressDialog.dismiss();
+        polylinePaths = new ArrayList<>();
+        originMarkers = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
 
+        for (Route route : routes) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+            ((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
+            ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);
+
+            originMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.start_blue))
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+            destinationMarkers.add(mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.end_green))
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions().
+                    geodesic(true).
+                    color(Color.BLUE).
+                    width(15);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add(mMap.addPolyline(polylineOptions));
+        }
     }
+
+    @Override
+    public void onPostExecute(Object o, String action) {
+        parkingDTOS = (ArrayList<ParkingDTO>) o;
+        ParkingDTO parkingDTO = parkingDTOS.get(0);
+
+//        mPreferences = getSharedPreferences("driver", 0);
+        GPSTracker gps = new GPSTracker(this);
+//        String directionLat = mPreferences.getString("parkingLat", "");
+//        String directionLng = mPreferences.getString("parkingLng", "");
+
+//        Log.e("TOA DO DIEM DEN: ", gps.getLatitude() + "---" +  gps.getLongitude());
+
+//        String ori = directionLat + "," + directionLng;
+        try {
+            new DirectionFinder(this, gps.getLatitude() + "," + gps.getLongitude(), parkingDTO.getLatitude() + "," + parkingDTO.getLongitude()).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
