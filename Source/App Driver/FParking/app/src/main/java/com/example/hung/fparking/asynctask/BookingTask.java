8ce44d4481ae3307fd8c67b1f2b847ad02923321
KhoiND <khoindse04028@fpt.edu.vn>
2018-07-26 01:38:55 +0700

    KhoiND

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/asynctask/BookingTask.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/asynctask/BookingTask.java
@@ -189,6 +189,61 @@ class CreateBooking extends AsyncTask<Void, Void, Boolean> {
     }
 }
 
+class GetBookingTaskWhenOrder extends AsyncTask<Void, Void, Boolean> {
 
+    private String bookingID, action;
+    private ArrayList<BookingDTO> booking;
+    private IAsyncTaskHandler container;
+
+    public GetBookingTaskWhenOrder(String bookingID, String action, IAsyncTaskHandler container) {
+        this.bookingID = bookingID;
+        this.action = action;
+        this.container = container;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        booking = new ArrayList<>();
+        HttpHandler httpHandler = new HttpHandler();
+        try {
+            String json = httpHandler.get(Constants.API_URL + "bookings/" + bookingID);
+            Log.e("toa do: ", Constants.API_URL + "bookings/" + bookingID);
+//            JSONArray jsonArray = new JSONArray(json);
+
+//            for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject c = new JSONObject(json);
+            int bookingID = c.getInt("id");
+            Double price = c.getDouble("price");
+            int status = c.getInt("status");
+            String timein = c.getString("timein");
+            String timeout = c.getString("timeout");
+            double amount = c.getDouble("amount");
+            double comission = c.getDouble("comission");
+            double totalfine = c.getDouble("totalfine");
+
+            JSONObject parking = c.getJSONObject("parking");
+            int parkingID = parking.getInt("id");
+            String address = parking.getString("address");
+
+            JSONObject drivervehicle = c.getJSONObject("drivervehicle");
+            JSONObject vehicle = drivervehicle.getJSONObject("vehicle");
+            int vehicleID = vehicle.getInt("id");
+            String licenseplate = vehicle.getString("licenseplate");
+
+            booking.add(new BookingDTO(bookingID, vehicleID, parkingID, address, timein, timeout, price, status, licenseplate, amount, comission, totalfine));
+//            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+        container.onPostExecute(booking, action);
+    }
+}
 
 
