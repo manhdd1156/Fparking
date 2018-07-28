55a3a90bb3b41542aa0bd83d2ec00aed9dd0ebf4
Hung Nguyen <hungnvse04023@fpt.edu.vn>
2018-07-29 03:19:21 +0700

    update HN

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
@@ -1,13 +1,30 @@
 package com.example.hung.fparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DriverInformation extends AppCompatActivity {
-
+ImageView backInformation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_information);
+        backInformation = findViewById(R.id.imageViewBackInformation);
+        backInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentInformation = new Intent(DriverInformation.this, HomeActivity.class);
+                startActivity(intentInformation);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
