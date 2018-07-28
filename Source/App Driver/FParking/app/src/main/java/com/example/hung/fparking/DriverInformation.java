96dbe5663208217b6580cf805cb42202bd26dd7b
Hung Nguyen <hungnvse04023@fpt.edu.vn>
2018-07-29 02:39:25 +0700

    update HN

---
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
@@ -0,0 +1,13 @@
+package com.example.hung.fparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_information);
+    }
+}
