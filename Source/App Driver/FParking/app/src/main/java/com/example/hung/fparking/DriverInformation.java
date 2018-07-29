b9a6b44cf84d3af7868cb3836dd12eab1c900292
Hung Nguyen <hungnvse04023@fpt.edu.vn>
2018-07-29 13:05:23 +0700

    update information HN

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
@@ -1,18 +1,33 @@
 package com.example.hung.fparking;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class DriverInformation extends AppCompatActivity {
 ImageView backInformation;
+    Button mUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_information);
         backInformation = findViewById(R.id.imageViewBackInformation);
+        mUpdate = (Button) findViewById(R.id.buttonUpdate);
+        mUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(DriverInformation.this);
+                View mView = getLayoutInflater().inflate(R.layout.activity_dialog_confirm_pass,null);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
+
         backInformation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
