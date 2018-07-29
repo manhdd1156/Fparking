e3db077b21d266b36ecf4ef884e5cb04f203dbb3
KhoiND <khoindse04028@fpt.edu.vn>
2018-07-29 17:35:14 +0700

    KhoiND
    fix lỗi + đợi service

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/DriverInformation.java
@@ -6,25 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class DriverInformation extends AppCompatActivity {
-ImageView backInformation;
-    Button mUpdate;
+
+    ImageView backInformation;
+    Button mUpdate, confirm;
+    EditText password, tbName, tbPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_information);
+
+        // tạo dialog
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(DriverInformation.this);
+        View mView = getLayoutInflater().inflate(R.layout.activity_dialog_confirm_pass, null);
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+
+        // ánh xạ
         backInformation = findViewById(R.id.imageViewBackInformation);
         mUpdate = (Button) findViewById(R.id.buttonUpdate);
+        tbName = findViewById(R.id.tbName);
+        tbPhone = findViewById(R.id.tbPhone);
+        confirm = mView.findViewById(R.id.btnLogin);
+        password = mView.findViewById(R.id.tbPass);
+
+        // listener
         mUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(DriverInformation.this);
-                View mView = getLayoutInflater().inflate(R.layout.activity_dialog_confirm_pass,null);
-                mBuilder.setView(mView);
-                final AlertDialog dialog = mBuilder.create();
                 dialog.show();
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tbPhone.setText(password.getText());
+                dialog.cancel();
             }
         });
 
@@ -37,6 +59,7 @@ ImageView backInformation;
             }
         });
     }
+
     @Override
     public void finish() {
         super.finish();
