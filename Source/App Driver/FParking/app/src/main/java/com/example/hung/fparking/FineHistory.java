88bcce43b7df37f5799804be5718a312be3437c2
Hung Nguyen <hungnvse04023@fpt.edu.vn>
2018-08-04 01:59:33 +0700

    update

--- Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/FineHistory.java
+++ Source/App Driver/FParking/app/src/main/java/com/example/hung/fparking/FineHistory.java
@@ -1,13 +1,27 @@
 package com.example.hung.fparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FineHistory extends AppCompatActivity {
-
+  ImageView backFineHistory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fine_history);
+
+        backFineHistory = findViewById(R.id.imageViewBackFineList);
+
+        backFineHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backFine = new Intent(FineHistory.this, HomeActivity.class);
+                startActivity(backFine);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
     }
 }
