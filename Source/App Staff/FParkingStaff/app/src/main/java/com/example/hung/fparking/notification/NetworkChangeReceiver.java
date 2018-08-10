0ea27f895093c4b490d2094275ae766e75ca4520
Duy Manh <manhdd1156@gmail.com>
2018-08-10 23:08:14 +0700

    dsadsa

---
+++ Source/App Staff/FParkingStaff/app/src/main/java/com/example/hung/fparking/notification/NetworkChangeReceiver.java
@@ -0,0 +1,15 @@
+package com.example.hung.fparking.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println("dsaaaaaaaaaaaaaaaa");
+        String status = NetworkUltil.getConnectivityStatusString(context);
+        Toast.makeText(context, status, Toast.LENGTH_LONG).show();
+    }
+}
