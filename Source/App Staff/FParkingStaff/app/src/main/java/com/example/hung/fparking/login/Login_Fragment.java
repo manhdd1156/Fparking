4ec3e9ba364be16aa869e08b6786438b2c60b565
Duy Manh <manhdd1156@gmail.com>
2018-08-12 02:34:08 +0700

    sdasdas

--- Source/App Staff/FParkingStaff/app/src/main/java/com/example/hung/fparking/login/Login_Fragment.java
+++ Source/App Staff/FParkingStaff/app/src/main/java/com/example/hung/fparking/login/Login_Fragment.java
@@ -53,7 +53,7 @@ public class Login_Fragment extends Fragment implements OnClickListener, IAsyncT
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.layout_login, container, false);
+        view = inflater.inflate(R.layout.activity_login, container, false);
 
         initViews();
         setListeners();
